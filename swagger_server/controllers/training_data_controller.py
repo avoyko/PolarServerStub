d5dfc41089f41e06cde2da229b180247a5d14c6a
Войко Андрей Александрович <aavoyko@edu.hse.ru>
2024-04-25 14:46:45 +0300

    modify test

--- swagger_server/controllers/training_data_controller.py
+++ swagger_server/controllers/training_data_controller.py
@@ -36,7 +36,7 @@ def create_exercise_transaction(user_id):  # noqa: E501
     :rtype: TransactionLocation
     """
     return TransactionLocation(122,
-    "http://localhost:8080/v3/users/21/physical-information-transactions/32")
+    "http://localhost:8080/v3/users/21/exercise-transactions/32")
 
 
 def get_available_samples(user_id, transaction_id, exercise_id):  # noqa: E501
@@ -53,7 +53,7 @@ def get_available_samples(user_id, transaction_id, exercise_id):  # noqa: E501
 
     :rtype: Samples
     """
-    return 'do some magic!'
+    return Samples()
 
 
 def get_exercise_summary(user_id, transaction_id, exercise_id):  # noqa: E501
